6218406be35557688a3d4a00bb1c2ca14013c737
Khurram.Ahmed <khurram.ahmed@emaratech.ae>
2022-07-15 09:09:07 +0400

    TDVOP-87103 TRKSRV-122488 Updated Pom.xml and added hashCode,Equals and ToString methods

--- platform-lookup-service/src/main/java/com/emaratech/platform/lookupsvc/model/VisaType.java
+++ platform-lookup-service/src/main/java/com/emaratech/platform/lookupsvc/model/VisaType.java
@@ -2,16 +2,19 @@ package com.emaratech.platform.lookupsvc.model;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.StringJoiner;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang3.builder.EqualsBuilder;
 
 @Getter
 @Setter
 /**
- *
+ * VisaType Class for holding the VisaType data.
  */
 public class VisaType implements Serializable {
 
@@ -62,6 +65,58 @@ public class VisaType implements Serializable {
 
     private BigDecimal isValidityFromIssueDate;
 
-    private BigDecimal IsValidityFromEntryDate;
+    private BigDecimal isValidityFromEntryDate;
+
+
+    /**
+     * Checks the equality between two {@code visaType} objects.
+     *
+     * @param visaType {@code visaType} object
+     * @return true if both the objects are equal, false otherwise
+     */
+    @Override
+    public boolean equals(Object visaType) {
+        if (this == visaType) {
+            return true;
+        }
+
+        if (visaType == null || getClass() != visaType.getClass()) {
+            return false;
+        }
+
+        VisaType that = (VisaType) visaType;
+
+        return new EqualsBuilder()
+                .append(visaTypeId, that.visaTypeId)
+                .append(visaTypeNameEn, that.visaTypeNameEn)
+                .append(visaTypeNameAr, that.visaTypeNameAr)
+                .append(isBorderVisa, that.isBorderVisa)
+                .isEquals();
+    }
+
+    /**
+     * Returns the hashcode of the {@code VisaType} object.
+     *
+     * @return hashcode of the {@code VisaType} object
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(visaTypeId, visaTypeNameEn);
+    }
+
+    /**
+     * Returns the {@code String} representation of {@code visaType} object.
+     *
+     * @return the string representation of the object.
+     */
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", VisaType.class.getSimpleName() + "(", ")")
+                .add("visaTypeId=" + visaTypeId + "")
+                .add("visaTypeNameEn=" + visaTypeNameEn + "")
+                .add("visaTypeNameAr=" + visaTypeNameAr + "")
+                .add("isBorderVisa=" + isBorderVisa + "")
+                .toString();
+    }
 
 }
